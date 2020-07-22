383e2d1c8610c2bf405e72bd0ef9cd3c4db2def9
houce_ext <houce_ext@cop.cmsoft.com.cn>
2020-07-22 09:36:00 +0800

    第一次提交

---
+++ imooc-security-browser/src/main/java/com/imooc/security/browser/BrowserSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.imooc.security.browser;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Security Web请求配置
+ *
+ * @author Created By Houce
+ * @date 2020-07-17 15:52
+ */
+@Configuration
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
